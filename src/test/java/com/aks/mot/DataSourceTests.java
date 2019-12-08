75f1c3759d3381dd4f4a168077ccf484c2444d0d
김동현 <@DESKTOP-I1MVIPM>
2019-12-08 09:41:19 +0900

    commit

--- src/test/java/com/aks/mot/DataSourceTests.java
+++ src/test/java/com/aks/mot/DataSourceTests.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.aks.mot.com.config.ApplicationConfig;
+import com.aks.mot.cmn.config.ApplicationConfig;
 
 import lombok.Setter;
 
